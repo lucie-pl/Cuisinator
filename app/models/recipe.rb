3d87bad52da7858599ff861b92a0b8aaf23e3115
Mathieu Audat <mathieuaudat@gmail.com>
2024-01-19 18:18:53 -0500

    models: link recipes and ing/qt/units with jtable
    Now Ingredients, Quantities and Unities are linked to Recipes through
    the joined table, with explicit keys.
    
    Add function to create these 3 tables through RI.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients, foreign_key: :ingredient_id
+  has_many :quantities, through: :recipe_ingredients, foreign_key: :quantity_id
+  has_many :unities, through: :recipe_ingredients, foreign_key: :unity_id
   accepts_nested_attributes_for :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredients
   # has_many :my_recipes
   # has_many :users, through: :my_recipes
 
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,6 +3,9 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
   belongs_to :unity
   belongs_to :quantity
+  accepts_nested_attributes_for :ingredient
+  accepts_nested_attributes_for :quantity
+  accepts_nested_attributes_for :unity
 
-  validates :recipe_id, :ingredient_id, :unity_id, :quantity_id, presence: true
+  #validates :recipe_id, :ingredient_id, :unity_id, :quantity_id, presence: true
 end
