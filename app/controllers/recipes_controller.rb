f2fc8f509b2531d2a5dbeaef9ed3040f2e70fa0c
Lucie Plantier <lucie.plantier@live.fr>
2024-01-27 17:45:20 -0500

    Update of recipes controller, views and forms
    - update in the recipe_ingredients params
    - new forms with partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,39 +1,30 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show]
+  before_action :set_recipe, only: [:show, :edit, :update]
   def index
     @recipes = Recipe.all
   end
 
   def new
     @recipe = Recipe.new
-    @recipe.recipe_ingredients.build
+    ri = @recipe.recipe_ingredients.build
+    ri.build_ingredient(params[:recipe_ingredients_attributes])
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
-    # @recipe.id = params[:recipe_ingredient_id]
     if @recipe.save
-      ing = Ingredient.create(name: params[:recipe][:ingredients][:name])
-      q = Quantity.create(quantity: params[:recipe][:quantities][:quantity])
-      u = Unity.create(unity: params[:recipe][:unities][:unity])
-
-      @recipe.recipe_ingredients.create(
-          recipe_id: @recipe.id,
-          ingredient_id: ing.id,
-          unity_id: u.id,
-          quantity_id: q.id
-        )
       redirect_to @recipe, notice: 'Recipe was succesfully created'
     else
       render :new
     end
   end
 
-  # def edit
-  # end
 
-  # def update
-  # end
+  def update
+    @recipe.update(recipe_params)
+    debugger
+    redirect_to recipe_path(@recipe)
+  end
 
   # def destroy
   # end
@@ -54,11 +45,10 @@ class RecipesController < ApplicationController
         :id,
         :recipe_id,
         :ingredient_id,
-        :quantity_id,
-        :unity_id,
+        :quantity,
+        :unity,
+        :_destroy,
         ingredient_attributes: [:name],
-        quantity_attributes: [:quantity],
-        unity_attributes: [:unity]
       ]
     )
   end
--- test/models/recipe_ingredient_test.rb
+++ test/models/recipe_ingredient_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class RecipeIngredientTest < ActiveSupport::TestCase
-  test "should have a recipe id, ingredient id, unity id and quantity id" do
+  test "should have a recipe id, ingredient id, a unity and a quantity" do
     r_i = recipe_ingredients(:one)
     assert r_i.valid?
   end
