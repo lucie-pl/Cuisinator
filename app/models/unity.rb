fc1703dbff1c1329f9c1e65fe59db45720aab836
Lucie Plantier <lucie.plantier@live.fr>
2024-01-27 17:38:08 -0500

    Delete unecessary controllers/models/test/migratio

--- app/models/unity.rb
+++
@@ -1,6 +0,0 @@
-class Unity < ApplicationRecord
-  has_many :recipe_ingredients
-  has_many :recipes, through: :recipe_ingredients
-
-  validates :unity, presence: true, inclusion: { in: %w(ml cl l g kg cup tsp Tbsp) }
-end
--- test/controllers/recipe_ingredients_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class RecipeIngredientsControllerTest < ActionDispatch::IntegrationTest
-  test "recipe_pain is valid" do
-    r = recipe_ingredient(:ri_pain)
-    assert r.valid?
-  end
-end
