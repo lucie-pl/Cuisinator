3c508b29b356f8a5fb40aed1e45a9086c76a21a6
Lucie Plantier <lucie.plantier@live.fr>
2024-04-10 13:58:51 -0400

    Migration of User model with username, first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_151722) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
