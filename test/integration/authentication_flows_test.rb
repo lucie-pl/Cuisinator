1b39bd4489709811139836719335052be36bafe9
Mathieu Audat <mathieuaudat@gmail.com>
2024-04-20 19:00:23 -0400

    test/integration: add a few placeholder tests
    These tests will be there to validate the flow of
    an annonymous or authenticated user.

---
+++ test/integration/authentication_flows_test.rb
@@ -0,0 +1,15 @@
+class PostsTests < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+
+    test 'anonymous user can see home page' do
+        assert false, "Test not implemented yet"
+    end
+
+    test 'anonymous user cannot see other pages than home' do
+        assert false, "Test not implemented yet"
+    end
+
+    test 'authenticated user can see pages like recipes' do
+        assert false, "Test not implemented yet"
+    end
+  end
