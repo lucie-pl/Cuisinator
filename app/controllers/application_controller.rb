28155733883e5edb5dd3e9198d7e34eed102e288
Lucie Plantier <lucie.plantier@live.fr>
2024-04-23 19:21:03 -0400

    Commenting out Devise for the moment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :email, :password])
-    # TODO devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
+  # def configure_permitted_parameters
+  #   # For additional fields in app/views/devise/registrations/new.html.erb
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :email, :password])
+  #   # TODO devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
-  end
+  #   # For additional in app/views/devise/registrations/edit.html.erb
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
+  # end
 end
