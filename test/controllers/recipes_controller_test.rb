604bd6d5d262c7bf1d1c6d54c227a7638690114d
Lucie Plantier <lucie.plantier@live.fr>
2024-04-24 15:33:48 -0400

    Running reek fixes

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   test 'sandwich au jambon has 2 RI pain and jambon' do
-    q = recipes(:sandwich_jambon)
-    assert q.valid?
+    recipe = recipes(:sandwich_jambon)
+    assert recipe.valid?
   end
 end
